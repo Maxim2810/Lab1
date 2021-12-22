9d215a7294101e3e61829e7ec2007c1deeda066a
GhostGuy <ghostguy@bk.ru>
2021-12-23 03:05:39 +0700

    Init

---
+++ app/src/main/java/com/example/lab1/MyClass.java
@@ -0,0 +1,20 @@
+package com.example.lab1;
+
+public class MyClass {
+    public static int max(int ... vals) {
+        int max = vals[0];
+        for (int i : vals) {
+            if (i > max)
+                max = i;
+        }
+        return max;
+    }
+    public static int min(int ... vals) {
+        int min = vals[0];
+        for (int i : vals) {
+            if (i < min)
+                min = i;
+        }
+        return min;
+    }
+}
